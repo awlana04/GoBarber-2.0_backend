c73c410ea9849ab7e7a925f774da1edc0d30968b
awlana04 <cawlana040@gmail.com>
2021-03-08 16:36:22 -0300

    appointments routes

---
+++ backend/src/models/Appointments.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: String;
+
+  @Column()
+  provider: String;
+
+  @Column('time with time zone')
+  date: Date;
+}
+
+export default Appointment;
