40041118ffdf2ca8dadf918c9e259ed69a096f2b
awlana04 <cawlana040@gmail.com>
2021-03-12 22:40:07 -0300

    nextjs

--- backend/src/shared/infra/http/server.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloServer } from 'apollo-server';
-
-import schema from '../graphql/index';
-import { createContext } from './context';
-
-const app = new ApolloServer({ schema, context: createContext })
-
-app.listen().then(() => console.log('🚀 Server started on port 4000'));
