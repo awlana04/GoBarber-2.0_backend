f82102bd42ec06ea81ddca40a6af0f7f200e443e
awlana04 <cawlana040@gmail.com>
2021-03-17 17:54:34 -0300

    authentication

--- web/pages/api/auth/[...nextauth].ts
+++ web/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default (request, response) => NextAuth(request, response, {
   providers: [
@@ -13,4 +17,13 @@ export default (request, response) => NextAuth(request, response, {
   jwt: {
     secret: process.env.JWT_SECRET,
   },
+  adapter: Adapters.Prisma.Adapter({
+    prisma,
+    modelMapping: {
+      User: 'user',
+      Account: 'account',
+      Session: 'session',
+      VerificationRequest: 'verificationRequest'
+    }
+  })
 });
