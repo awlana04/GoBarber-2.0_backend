43571b066b6493beb8f3a96123b228c8448d10ee
awlan04 <cawlana040@gmail.com>
2021-04-25 23:38:53 -0300

    database updated to new features

--- web/pages/api/nexus/schemas/Mutation.ts
+++
@@ -1,9 +0,0 @@
-import { mutationType } from 'nexus';
-
-export const Mutation = mutationType({
-  definition(t) {
-    t.crud.createOneUser(),
-      t.crud.updateOneUser(),
-      t.crud.deleteOneUser()
-  }
-});
