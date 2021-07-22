d75c3fd79ceef322ef28fada35b47ea41b3f7f40
awlan04 <cawlana040@gmail.com>
2021-07-21 22:36:20 -0300

    error handling

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -20,22 +20,20 @@ appointmentRouter.get('/', async (request, response) => {
 });
 
 appointmentRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const { provider_id, date } = request.body;
 
-    const createAppointment = new CreateAppointmentService();
+  const parsedDate = parseISO(date);
 
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parsedDate
-    })
+  const createAppointment = new CreateAppointmentService();
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parsedDate
+  })
+
+  return response.json(appointment);
+
 })
 
 export default appointmentRouter;
