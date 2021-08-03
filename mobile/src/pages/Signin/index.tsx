e6f2e91c730a234223ccb6519465d70dc9a06c52
awlan04 <cawlana040@gmail.com>
2021-08-03 00:57:37 -0300

    signin page

--- mobile/src/pages/Signin/index.tsx
+++ mobile/src/pages/Signin/index.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { Image } from 'react-native';
+import Icon from '@expo/vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import Logo from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 const Signin: React.FC = () => {
   return (
-    <Container>
-      <Image source={Logo} />
+    <>
+      <Container>
+        <Image source={Logo} />
 
-      <Title>Faça seu logon</Title>
+        <Title>Faça seu logon</Title>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button onPress={() => { }}>Entrar</Button>
-    </Container>
+        <Button onPress={() => { }}>Entrar</Button>
+
+        <ForgotPassword onPress={() => { }}>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccountButton onPress={() => { }}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
--- mobile/src/pages/Signin/styles.ts
+++ mobile/src/pages/Signin/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   padding: 0 30px;
@@ -14,4 +15,38 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-size: 24px;
   font-family: 'RobotoSlab_500Medium';
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 18px;
+  font-family: 'RobotoSlab_400Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  border-top-width: 1px;
+  border-color: #232129;
+
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  margin-left: 16px;
+
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab_400Regular';
 `;
