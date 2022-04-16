d35fe471f8959238d7410affba94a456c5c7b07d
Awlana Costa <cawlana040@gmail.com>
2022-04-15 21:43:55 -0300

    create barber

---
+++ backend/src/controllers/CreateBarberController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+
+import prisma from '../database/prisma';
+
+import AppError from '../utils/AppError';
+
+export class CreateBarberController {
+  public async execute(request: Request, response: Response) {
+    try {
+      const {
+        name,
+        location,
+        description,
+        images,
+        openAtNight,
+        openOnWeekends,
+      } = request.body;
+
+      const { id } = request.params;
+
+      const checkIfUserExists = await prisma.user.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (!checkIfUserExists) {
+        throw new AppError('User does not exists');
+      }
+
+      const barber = await prisma.barber.create({
+        data: {
+          name,
+          location,
+          description,
+          images,
+          openAtNight,
+          openOnWeekends,
+          user: {
+            connect: {
+              id,
+            },
+          },
+        },
+      });
+
+      return response.json(barber);
+    } catch (error) {
+      return response.json(error);
+    }
+  }
+}
