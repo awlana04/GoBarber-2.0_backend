088f471d8ebf4bf61dc9bf4e56471be566531f76
Awlana Costa <cawlana040@gmail.com>
2022-04-29 20:12:53 -0300

    test name and password entity domain

---
+++ backend/src/domain/entities/domain/name.spec.ts
@@ -0,0 +1,39 @@
+import Name from './name';
+
+describe('Name entity domain', () => {
+  it('should NOT be able to create a new name with an invalid name (blank name)', () => {
+    const invalidName = '';
+
+    const response = Name.create(invalidName).value as Error;
+
+    expect(response.name).toEqual('InvalidNameError');
+    expect(response.message).toEqual('Invalid name: ' + invalidName + '.');
+  });
+
+  it('should NOT be able to create a new name with an invalid name (too few characters)', () => {
+    const invalidName = 'a';
+
+    const response = Name.create(invalidName).value as Error;
+
+    expect(response.name).toEqual('InvalidNameError');
+    expect(response.message).toEqual('Invalid name: ' + invalidName + '.');
+  });
+
+  it('should NOT be able to create a new name with an invalid name (too many characters)', () => {
+    const invalidName = 'a'.repeat(129);
+
+    const response = Name.create(invalidName).value as Error;
+
+    expect(response.name).toEqual('InvalidNameError');
+    expect(response.message).toEqual('Invalid name: ' + invalidName + '.');
+  });
+
+  it('should be able to create a new name', () => {
+    const name = 'John Doe';
+
+    const response = Name.create(name).value as Name;
+
+    expect(response.value).toEqual(name);
+    expect(response).toBeInstanceOf(Name);
+  });
+});
---
+++ backend/src/domain/entities/domain/password.spec.ts
@@ -0,0 +1,45 @@
+import Password from './password';
+
+describe('Password entity domain', () => {
+  it('should NOT be able to create a new password with an invalid password (blank password)', () => {
+    const invalidPassword = '';
+
+    const response = Password.create(invalidPassword).value as Error;
+
+    expect(response.name).toEqual('InvalidPasswordError');
+    expect(response.message).toEqual(
+      'Invalid password: ' + invalidPassword + '.'
+    );
+  });
+
+  it('should NOT be able to create a new password with an invalid password (too few characters)', () => {
+    const invalidPassword = '123';
+
+    const response = Password.create(invalidPassword).value as Error;
+
+    expect(response.name).toEqual('InvalidPasswordError');
+    expect(response.message).toEqual(
+      'Invalid password: ' + invalidPassword + '.'
+    );
+  });
+
+  it('should NOT be able to create a new password with an invalid password (too many characters)', () => {
+    const invalidPassword = '123'.repeat(129);
+
+    const response = Password.create(invalidPassword).value as Error;
+
+    expect(response.name).toEqual('InvalidPasswordError');
+    expect(response.message).toEqual(
+      'Invalid password: ' + invalidPassword + '.'
+    );
+  });
+
+  it('should be able to create a new password', () => {
+    const password = '12345678';
+
+    const response = Password.create(password).value as Password;
+
+    expect(response.value).toEqual(password);
+    expect(response).toBeInstanceOf(Password);
+  });
+});
